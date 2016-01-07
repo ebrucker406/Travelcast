0d7e5937b5c33312d62c7c212e7b81a741882c51
Teyler7 <teyler.halama@gmail.com>
2016-01-06 22:03:03 -0700

    Importing Mapkit Working
    The Map now works

--- Travelcast/ViewController.swift
+++ Travelcast/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
